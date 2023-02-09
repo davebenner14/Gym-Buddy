03486e36bb094eb7fdb0d1d5ffb589d639ee6cb1
Mohamed Abdulle <m7assan95@gmail.com>
2023-02-08 21:53:44 -0500

    exercise modelcomplete

--- gymcollector/main_app/models.py
+++ gymcollector/main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Exercise(models.Model):
+    Type of workout = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    description = models.TextField(max_length=250)
