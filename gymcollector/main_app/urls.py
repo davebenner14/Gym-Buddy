e9e06f28aedfa3de1c1b1a20984b3485255d800c
David Benner <davidbenner@davids-mbp.lan>
2023-02-08 20:36:43 -0500

    added gym index to views.py and urls.py

--- gymcollector/main_app/urls.py
+++ gymcollector/main_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('', views.home, name='home'),
+  path('gyms/', views.gyms_index, name='index'),
 
 ]
 
--- gymcollector/main_app/views.py
+++ gymcollector/main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def gyms_index(request):
+  return render(request, 'gyms/index.html', {
+    'gyms': gyms
+  })
