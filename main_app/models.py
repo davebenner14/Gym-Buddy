79b85506d8d2173abfe577408cbe21f36eaebe7a
Mohamed Abdulle <m7assan95@gmail.com>
2023-02-10 22:07:34 -0500

    exercise changed

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Exercise(models.Model):
     TypeOfWorkout = models.CharField(max_length=100)
@@ -10,6 +10,10 @@ class Exercise(models.Model):
 
     def __str__(self):
         return self.TypeOfWorkout
+
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'cat_id': self.id})
     
 
 class Plan(models.Model):
