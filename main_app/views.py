7480b3945cd1d5af05802bba5de171aec997bb01
David Benner <davidbenner@davids-mbp.lan>
2023-02-11 20:49:37 -0500

    basic add plan completed

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-from .models import Exercise, Plan, Meal
+from .models import Exercise, Plan
 
 # Create your views here.
 def home(request):
@@ -11,40 +10,21 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-# def exercises_index(request):
-#   exercises = Exercise.objects.all()
-#   return render(request, 'exercises/index.html', {
-#     'exercises': exercises
-#   })
+def exercises_index(request):
+  exercises = Exercise.objects.all()
+  return render(request, 'exercises/index.html', {
+    'exercises': exercises
+  })
 
 
-# def exercises_detail(request, exercise_id):
-#   exercise = Exercise.objects.get(id=exercise_id)
-#   return render(request, 'exercises/detail.html', { 'exercise': exercise})
+def exercises_detail(request, exercise_id):
+  exercise = Exercise.objects.get(id=exercise_id)
+  return render(request, 'exercises/detail.html', { 'exercise': exercise})
 
 
 class ExerciseCreate(CreateView):
   model = Exercise
   fields = '__all__'
-
-
-
-class ExerciseList(ListView):
-  model = Exercise
-
-
-
-class ExerciseDetail(DetailView):
-  model = Exercise
-
-class ExerciseUpdate(UpdateView):
-  model = Exercise
-  fields = '__all__'
-
-class ExerciseDelete(DeleteView):
-  model = Exercise
-  success_url = '/exercises' 
-
 
 def plans_index(request):
   plans = Plan.objects.all()
@@ -58,33 +38,12 @@ def plans_detail(request, plan_id):
 
 class PlanCreate(CreateView):
   model = Plan
-  fields = '__all__'
-  success_url = '/plans/{plan_id}'
+  fields = ['name', 'weight', 'goal']
 
 class PlanUpdate(UpdateView):
   model = Plan
   fields = ['name', 'weight', 'goal']
 
-class PlanDelete(DeleteView):
-   model = Plan
-   success_url = '/plans'
-
-class MealList(ListView):
-  model = Meal
-
-class MealCreate(CreateView):
-  model = Meal
-  fields = '__all__'
-
-class MealDetail(DetailView):
-  model = Meal
-
-class MealUpdate(UpdateView):
-  model = Meal
-  fields = ['name', 'price']
-
-class MealDelete(DeleteView):
-  model = Meal
-  success_url = '/meals'
-
-
+class PlantDelete(DeleteView):
+  model = Plan
+  success_url = '/plans'
