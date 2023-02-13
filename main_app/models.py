2543bb6314b2e0492758a53272994c980e760fc4
Tin Ian Lo <lotinian@Tindebijixingdiannao.local>
2023-02-13 15:56:01 -0500

    finsih m to m for health- meal

--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,20 @@ from django.db import models
 from django.urls import reverse
 
 
+
 # Create your models here.
+class Meal(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('meals_detail', kwargs={'pk': self.id})
+
 class Exercise(models.Model):
     TypeOfWorkout = models.CharField(max_length=100)
     duration = models.IntegerField()
@@ -22,6 +35,10 @@ class Plan(models.Model):
     name = models.CharField(max_length=100)
     weight = models.IntegerField()
     goal = models.TextField(max_length=250)
+    meals = models.ManyToManyField(Meal)
+
+    
+
    
 
     def __str__(self):
@@ -30,14 +47,3 @@ class Plan(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'plan_id': self.id})
 
-class Meal(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('toys_detail', kwargs={'pk': self.id})
