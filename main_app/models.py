84a2c066b697ce7cfaabecb8cb5ca42170a2855d
David Benner <davidbenner@davids-mbp.lan>
2023-02-14 10:49:35 -0500

    craete registration/login.html and migrated files

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -22,6 +23,8 @@ class Plan(models.Model):
     name = models.CharField(max_length=100)
     weight = models.IntegerField()
     goal = models.TextField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
    
 
     def __str__(self):
