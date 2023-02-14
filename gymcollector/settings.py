1d2805e8d1da28ccbd94f9a9772883a843bf1c73
David Benner <davidbenner@davids-mbp.lan>
2023-02-14 11:08:50 -0500

    login page working

--- gymcollector/settings.py
+++ gymcollector/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = '/plans/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
