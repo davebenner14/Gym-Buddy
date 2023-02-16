2287939a8dc8f9fb37f052d9a546e1050f4df4ed
Tin Ian Lo <lotinian@Tindebijixingdiannao.local>
2023-02-16 12:41:59 -0500

    complete login

--- main_app/urls.py
+++ main_app/urls.py
@@ -27,6 +27,5 @@ urlpatterns = [
   path('meals/<int:meal_id>/add_photo/', views.add_photo_for_meal, name='add_photo_for_meal'),
   path('exercise/<int:exercise_id>/add_photo/', views.add_photo_for_exercise, name='add_photo_for_exercise'),
   path('accounts/signup/', views.signup, name='signup'),
-
 ]
 
